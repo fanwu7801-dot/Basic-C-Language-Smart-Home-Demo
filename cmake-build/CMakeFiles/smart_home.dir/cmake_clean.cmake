efe1e83f7915ac92f7ff220815f754fe4f7f99ba
fan_zx <fanwu7801.gmail.com>
2026-05-25 22:19:13 +0800

    Add CMake build files for smart_home project
    - Created cmake_clean.cmake for cleaning build artifacts.
    - Added compiler_depend.make and depend.make for dependency management.
    - Introduced flags.make to specify compilation flags and includes.
    - Generated includes_C.rsp for include directories.
    - Created link.txt and linkLibs.rsp for linking libraries.
    - Added objects1.rsp for object files.
    - Implemented progress.make to track build progress.
    - Generated Makefile for building the project with MinGW.
    - Created cmake_install.cmake for installation configuration and manifest generation.

---
+++ cmake-build/CMakeFiles/smart_home.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smart_home.dir/BSP/bsp_console.c.obj"
+  "CMakeFiles/smart_home.dir/BSP/bsp_console.c.obj.d"
+  "CMakeFiles/smart_home.dir/BSP/bsp_storage.c.obj"
+  "CMakeFiles/smart_home.dir/BSP/bsp_storage.c.obj.d"
+  "CMakeFiles/smart_home.dir/app/main.c.obj"
+  "CMakeFiles/smart_home.dir/app/main.c.obj.d"
+  "CMakeFiles/smart_home.dir/app/smart_home.c.obj"
+  "CMakeFiles/smart_home.dir/app/smart_home.c.obj.d"
+  "CMakeFiles/smart_home.dir/driver/air_conditioner.c.obj"
+  "CMakeFiles/smart_home.dir/driver/air_conditioner.c.obj.d"
+  "CMakeFiles/smart_home.dir/driver/device.c.obj"
+  "CMakeFiles/smart_home.dir/driver/device.c.obj.d"
+  "CMakeFiles/smart_home.dir/driver/light.c.obj"
+  "CMakeFiles/smart_home.dir/driver/light.c.obj.d"
+  "CMakeFiles/smart_home.dir/driver/security_alarm.c.obj"
+  "CMakeFiles/smart_home.dir/driver/security_alarm.c.obj.d"
+  "CMakeFiles/smart_home.dir/middleware/basic_ui.c.obj"
+  "CMakeFiles/smart_home.dir/middleware/basic_ui.c.obj.d"
+  "libsmart_home.dll.a"
+  "smart_home.exe"
+  "smart_home.exe.manifest"
+  "smart_home.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smart_home.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build/mingw-debug/CMakeFiles/smart_home.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/BSP/bsp_console.c" "CMakeFiles/smart_home.dir/BSP/bsp_console.c.obj" "gcc" "CMakeFiles/smart_home.dir/BSP/bsp_console.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/BSP/bsp_storage.c" "CMakeFiles/smart_home.dir/BSP/bsp_storage.c.obj" "gcc" "CMakeFiles/smart_home.dir/BSP/bsp_storage.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/app/main.c" "CMakeFiles/smart_home.dir/app/main.c.obj" "gcc" "CMakeFiles/smart_home.dir/app/main.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/app/smart_home.c" "CMakeFiles/smart_home.dir/app/smart_home.c.obj" "gcc" "CMakeFiles/smart_home.dir/app/smart_home.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/driver/air_conditioner.c" "CMakeFiles/smart_home.dir/driver/air_conditioner.c.obj" "gcc" "CMakeFiles/smart_home.dir/driver/air_conditioner.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/driver/device.c" "CMakeFiles/smart_home.dir/driver/device.c.obj" "gcc" "CMakeFiles/smart_home.dir/driver/device.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/driver/light.c" "CMakeFiles/smart_home.dir/driver/light.c.obj" "gcc" "CMakeFiles/smart_home.dir/driver/light.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/driver/security_alarm.c" "CMakeFiles/smart_home.dir/driver/security_alarm.c.obj" "gcc" "CMakeFiles/smart_home.dir/driver/security_alarm.c.obj.d"
+  "D:/c_porject4/Basic-C-Language-Smart-Home-Demo/middleware/basic_ui.c" "CMakeFiles/smart_home.dir/middleware/basic_ui.c.obj" "gcc" "CMakeFiles/smart_home.dir/middleware/basic_ui.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
